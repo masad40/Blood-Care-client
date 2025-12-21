09b64ef663ef923d04c6029e15a666e7648dc15d
masad40 <tasnifmasad40@gmail.com>
2025-12-22 01:23:34 +0600

    set up role

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -73,7 +73,7 @@ export const router = createBrowserRouter([
         element: <RequestDetails></RequestDetails>,
       },
       {
-        path: "updateProfile", // "/dashboard/updateProfile"
+        path: "updateProfile", 
         element: <UpdateProfile />,
       },
       {
